2eb953fed07071f91835fa61c90fcf3639a990a5
NicholasTavares <nicholasbalby@hotmail.com>
2022-03-29 16:10:38 -0300

    feat: get user service

---
+++ src/modules/users/services/FindUserService.ts
@@ -0,0 +1,21 @@
+import AppError from "@shared/errors/AppError";
+import { getCustomRepository } from "typeorm";
+import { IUser } from "../models/IUser";
+import UsersRepository from "../repositories/UsersRepository";
+
+class FindUserService {
+  public async execute(id: string): Promise<IUser> {
+    const usersRepository = getCustomRepository(UsersRepository)
+
+    const user = await usersRepository.findById(id)
+
+    if (!user) {
+      throw new AppError('User not found')
+    }
+
+    return user
+  }
+
+}
+
+export default FindUserService
