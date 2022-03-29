f8dfde516c15f145c5cc4ffc9c4d1593568c9415
NicholasTavares <nicholasbalby@hotmail.com>
2022-03-29 13:59:01 -0300

    feat: create user controller

---
+++ src/modules/users/controllers/UsersController.ts
@@ -0,0 +1,20 @@
+import { instanceToInstance } from "class-transformer";
+import { Request, Response } from "express";
+import CreateUserService from "../services/CreateUserService";
+
+export default class UsersController {
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body
+
+    const createUser = new CreateUserService()
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password
+    })
+
+    return response.json(instanceToInstance(user))
+  }
+}
