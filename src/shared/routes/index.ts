32f6a9a3961de6390da0b8d29b6a19dd7ee14a0d
NicholasTavares <nicholasbalby@hotmail.com>
2022-04-02 16:48:33 -0300

    feat: add books route

--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -1,8 +1,10 @@
+import booksRouter from '@modules/books/routes/books.routes'
 import usersRouter from '@modules/users/routes/users.routes'
 import { Router } from 'express'
 
 const routes = Router()
 
 routes.use('/users', usersRouter)
+routes.use('/books', booksRouter)
 
 export default routes
