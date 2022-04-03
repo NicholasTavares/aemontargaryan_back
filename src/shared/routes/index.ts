3b82795bca8966072a813fae683f93aa90d8bfc7
NicholasTavares <nicholasbalby@hotmail.com>
2022-04-03 20:34:24 -0300

    feat: add chapters routes

--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -1,4 +1,5 @@
 import booksRouter from '@modules/books/routes/books.routes'
+import chaptersRouter from '@modules/chapters/routes/chapters.routes'
 import usersRouter from '@modules/users/routes/users.routes'
 import { Router } from 'express'
 
@@ -6,5 +7,6 @@ const routes = Router()
 
 routes.use('/users', usersRouter)
 routes.use('/books', booksRouter)
+routes.use('/chapters', chaptersRouter)
 
 export default routes
