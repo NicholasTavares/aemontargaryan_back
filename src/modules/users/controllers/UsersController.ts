a707860a1624d900cc9526a99771b7713d852140
NicholasTavares <nicholasbalby@hotmail.com>
2022-04-03 20:35:37 -0300

    refactor: remove debug

--- src/modules/users/controllers/UsersController.ts
+++ src/modules/users/controllers/UsersController.ts
@@ -13,8 +13,6 @@ export default class UsersController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { name, email, password } = request.body
 
-    console.log(request.query)
-
     const createUser = new CreateUserService()
 
     const user = await createUser.execute({
