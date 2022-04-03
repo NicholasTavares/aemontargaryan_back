fdeb11265b2ff2021c8c842beb0c0ad16d058df6
NicholasTavares <nicholasbalby@hotmail.com>
2022-04-03 19:54:30 -0300

    refactor: remove unnecessary not null validation in softDelete book service

--- src/modules/books/services/SoftDeleteBookService.ts
+++ src/modules/books/services/SoftDeleteBookService.ts
@@ -1,16 +1,9 @@
-import AppError from "@shared/errors/AppError"
 import { getCustomRepository } from "typeorm"
 import BooksRepository from "../repositories/BooksRepository"
 
 class SoftDeleteBookService {
   public async execute(id: string) {
     const booksRepository = getCustomRepository(BooksRepository)
-
-    const book = await booksRepository.findById(id)
-
-    if (!book) {
-      throw new AppError('Book not found!')
-    }
 
     await booksRepository.softDelete(id)
   }
