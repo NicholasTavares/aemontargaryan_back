bc37044c97df41d81ceaebd6a1fe0aa0748627cf
NicholasTavares <nicholasbalby@hotmail.com>
2022-04-04 11:03:46 -0300

    feat: update chapter controller

--- src/modules/chapters/controllers/ChaptersController.ts
+++ src/modules/chapters/controllers/ChaptersController.ts
@@ -3,9 +3,10 @@ import { Request, Response } from "express";
 import CreateChapterService from "../services/CreateChapterService";
 import FindChapterService from "../services/FindChapterService";
 import ListChaptersService from "../services/ListChaptersService";
+import UpdateChapterService from "../services/UpdateChapterService";
 
 export default class ChaptersRepository {
-  
+
   public async create(request: Request, response: Response): Promise<Response> {
     const { name } = request.body
 
@@ -34,5 +35,19 @@ export default class ChaptersRepository {
     const chapters = await listChapters.execute()
 
     return response.json(instanceToInstance(chapters))
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params
+    const { name } = request.body
+
+    const updateChapter = new UpdateChapterService()
+
+    const chapter = await updateChapter.execute({
+      id,
+      name
+    })
+
+    return response.json(instanceToInstance(chapter))
   }
 }
