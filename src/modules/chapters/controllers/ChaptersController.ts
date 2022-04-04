94c5d54a28985ca136215c32dfc798460898c623
NicholasTavares <nicholasbalby@hotmail.com>
2022-04-04 10:43:07 -0300

    feat: find chapter controller

--- src/modules/chapters/controllers/ChaptersController.ts
+++ src/modules/chapters/controllers/ChaptersController.ts
@@ -1,6 +1,7 @@
 import { instanceToInstance } from "class-transformer";
 import { Request, Response } from "express";
 import CreateChapterService from "../services/CreateChapterService";
+import FindChapterService from "../services/FindChapterService";
 
 export default class ChaptersRepository {
   
@@ -12,6 +13,16 @@ export default class ChaptersRepository {
     const chapter = await createChapter.execute({
       name
     })
+
+    return response.json(instanceToInstance(chapter))
+  }
+
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params
+
+    const showChapter = new FindChapterService()
+
+    const chapter = await showChapter.execute(id)
 
     return response.json(instanceToInstance(chapter))
   }
