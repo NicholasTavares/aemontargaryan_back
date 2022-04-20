f3c5c951eddd49937bad57a39cedd01eb4f7ebe6
NicholasTavares <nicholasbalby@hotmail.com>
2022-04-20 19:42:07 -0300

    feat: role find route

---
+++ src/modules/roles/routes/roles.routes.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express';
+import { celebrate, Joi, Segments } from "celebrate";
+import isAuthenticated from '@shared/middlewares/isAuthenticated';
+import RolesController from '../controllers/RolesController';
+
+const rolesRouter = Router()
+const rolesController = new RolesController()
+
+rolesRouter.post(
+  '/', isAuthenticated,
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required()
+    }
+  }),
+  rolesController.create
+)
+
+rolesRouter.get(
+  '/:id', isAuthenticated,
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().required()
+    }
+  }),
+  rolesController.show
+)
+
+export default rolesRouter
