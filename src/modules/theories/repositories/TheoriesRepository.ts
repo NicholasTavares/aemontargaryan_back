b2cc543ee1c4005c1c0f2e314748a85fbbb2e0d4
NicholasTavares <nicholasbalby@hotmail.com>
2022-04-22 15:33:28 -0300

    feat: soft delete and find by id methods in theories repository

--- src/modules/theories/repositories/TheoriesRepository.ts
+++ src/modules/theories/repositories/TheoriesRepository.ts
@@ -1,6 +1,9 @@
+import AppError from "@shared/errors/AppError";
 import { EntityRepository, getRepository, Repository } from "typeorm";
 import Theory from "../entities/Theory";
 import { ICreateTheory } from "../models/ICreateTheory";
+import { IFindById } from "../models/IFindById";
+import { ISoftDelete } from "../models/ISoftDelete";
 import { ITheory } from "../models/ITheory";
 
 @EntityRepository(Theory)
@@ -23,9 +26,31 @@ export default class TheoriesRepository {
     return theoryCreated
   }
 
+  public async findById({ id }: IFindById): Promise<ITheory> {
+    const theory = await this.ormRepository.findOne(id)
+
+    if (!theory) {
+      throw new AppError('Theory not found')
+    }
+
+    return theory
+  }
+
   public async save(theory: ITheory): Promise<ITheory> {
     const theorySaved = await this.ormRepository.save(theory)
 
     return theorySaved
+  }
+
+  public async softDelete({ id }: ISoftDelete) {
+    const theory = await this.findById({
+      id
+    })
+
+    if (!theory) {
+      throw new AppError('Theory not found')
+    }
+
+    return this.ormRepository.softDelete(id)
   }
 }
