3e8f42b5ffd5a7a443bdaf4c6f55ca9c20656007
NicholasTavares <nicholasbalby@hotmail.com>
2022-04-23 10:37:21 -0300

    feat: list theories service

---
+++ src/modules/theories/services/ListTheoriesServices.ts
@@ -0,0 +1,18 @@
+import { getCustomRepository } from "typeorm";
+import { IFindById } from "../models/IFindById";
+import { ITheory } from "../models/ITheory";
+import TheoriesRepository from "../repositories/TheoriesRepository";
+
+class ListTheoriesService {
+  public async execute({id}: IFindById): Promise<ITheory> {
+    const theoriesRepository = getCustomRepository(TheoriesRepository)
+
+    const theory = await theoriesRepository.findById({
+      id
+    })
+
+    return theory
+  }
+}
+
+export default ListTheoriesService
